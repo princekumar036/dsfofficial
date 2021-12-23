d53954a3741a4a4510ad23ea4eb59988fec23fbc
Kumar <pkumarmj@amazon.com>
2021-12-24 02:46:16 +0530

    test2

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+
 $(document).ready(function() {
-    // if(screen.width < 1024) {
         $('#nav-toggler').click(function() {
             $('#nav-menu').slideToggle();
             $('.nav-toggler-open').toggle();
             $('.nav-toggler-close').toggle();
         })
-    // }   
 })
